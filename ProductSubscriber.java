d3544f2cfa3827cfc72c8bdac941af2d70d01303
Elijah Wines <wines.elijah@gmail.com>
2023-02-13 20:17:08 -0500

    implemented Observer to create ProductSubscriber

--- ProductSubscriber.java
+++ ProductSubscriber.java
@@ -1,3 +1,24 @@
 public class ProductSubscriber implements Observer {
     
+    private String name;
+    private Subject product;
+
+    public ProductSubscriber(String n) {
+        this.name = n;
+    }
+
+    @Override
+    public void update() {
+        String msg = (String) product.getUpdate(this);
+        if (msg == null) {
+            System.out.println(name + ":: no new message");
+        } else {
+            System.out.println(name + ":: Consuming Message::" + msg);
+        }
+    }
+
+    @Override
+    public void setSubject(Subject sub) {
+        this.product = sub;
+    }
 }
